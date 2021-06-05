b61d4beaeec71a990825bfa1fe4ba06cfb4f8fca
Nicolas <nicolas.yang.1@gmail.com>
2021-06-05 15:33:30 +0200

    Start php mode

---
+++ templates/base.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description"
+        content="Aymeline Yang - Buisness Developer - Graphic Designer - Wedding Planner - Demandez votre devis">
+    <title>Aymeline Yang</title>
+    <link rel="icon" type="image/png" href="./asset/img/logoAY.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@100;200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/mobile-style.css">
+</head>
+
+<body>
+  
+
+</body>
+
+</html>
